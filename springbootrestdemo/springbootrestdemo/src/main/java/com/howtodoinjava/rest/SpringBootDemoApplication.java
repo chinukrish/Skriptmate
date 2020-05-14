c1e990cad8ac0f93c44bd5910cc5209c4446d491
akhilajayakumar <65273884+akhilajayakumar@users.noreply.github.com>
2020-05-14 18:01:59 +0530

    multi

--- springbootrestdemo/springbootrestdemo/src/main/java/com/howtodoinjava/rest/SpringBootDemoApplication.java
+++ springbootrestdemo/springbootrestdemo/src/main/java/com/howtodoinjava/rest/SpringBootDemoApplication.java
@@ -1,1 +1,7 @@
-sample = src/main/java
+sample = \\src\\main\\resources\\Files\\sample.pdf
+sampleEdited = \\src\\main\\resources\\Files\\sampleEdited.pdf
+BarcodePDF=\\src\\main\\resources\\Files\\Barcode.pdf
+QRCodePDF=\\src\\main\\resources\\Files\\QRCode.pdf
+TestDoc=\\src\\main\\resources\\Files\\demo.docx
+TextCompareOld=\\src\\main\\resources\\Files\\TextCompareOld.pdf
+TextCompareNew=\\src\\main\\resources\\Files\\TextCompareNew.pdf
